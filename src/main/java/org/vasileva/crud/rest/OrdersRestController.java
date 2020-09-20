aaf5f74fd7765ca8662b8980bd3c9e3ddd4f6c35
Elena Vasileva <elenavw@gmail.com>
2020-09-20 20:18:51 +0400

    commit 6 Orders endpoint

--- src/main/java/org/vasileva/crud/rest/OrdersRestController.java
+++ src/main/java/org/vasileva/crud/rest/OrdersRestController.java
@@ -1,4 +1,76 @@
 package org.vasileva.crud.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.vasileva.crud.entity.Orders;
+import org.vasileva.crud.service.OrdersService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders/")
 public class OrdersRestController {
+
+    @Autowired
+    private OrdersService ordersService;
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Orders> getDish (@PathVariable("id") Long id) {
+        if (id == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Orders order = ordersService.getById(id);
+        if (order == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Orders> saveDish (@RequestBody @Valid Orders order) {
+        HttpHeaders headers = new HttpHeaders();
+        if (order == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        ordersService.save(order);
+        return new ResponseEntity<>(order, headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Orders> updateDish (@RequestBody @Valid Orders order) {
+        HttpHeaders headers = new HttpHeaders();
+        if (order == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        ordersService.save(order);
+        return new ResponseEntity<>(order, headers, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Orders> deleteDish (@PathVariable("id") Long id) {
+        if (id == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Orders order = ordersService.getById(id);
+        if (order == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        ordersService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Orders>> getAllOrders() {
+        List<Orders> orders = this.ordersService.getAll();
+
+        if (orders.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
 }
