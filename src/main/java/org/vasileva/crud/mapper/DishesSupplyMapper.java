a48f56317d6749d8f52bb595fc8b09894e531426
Elena Vasileva <elenavw@gmail.com>
2020-09-26 20:46:21 +0400

    commit 21 add mappers

---
+++ src/main/java/org/vasileva/crud/mapper/DishesSupplyMapper.java
@@ -0,0 +1,9 @@
+package org.vasileva.crud.mapper;
+
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface DishesSupplyMapper {
+}
