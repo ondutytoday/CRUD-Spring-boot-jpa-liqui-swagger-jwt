7067b633dc5060116f7e2bd19a790bfc2a5392df
Elena Vasileva <elenavw@gmail.com>
2020-09-26 22:29:17 +0400

    commit 27 SupplyRestController update

--- src/main/java/org/vasileva/crud/rest/SupplyRestController.java
+++ src/main/java/org/vasileva/crud/rest/SupplyRestController.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.vasileva.crud.dto.SupplyDto;
 import org.vasileva.crud.entity.Supply;
+import org.vasileva.crud.mapper.SupplyMapper;
 import org.vasileva.crud.service.SupplyService;
 
 import javax.validation.Valid;
@@ -18,9 +20,11 @@ public class SupplyRestController {
 
     @Autowired
     private SupplyService supplyService;
+    @Autowired
+    private SupplyMapper supplyMapper;
 
     @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Supply> getSupply (@PathVariable("id") Long id) {
+    public ResponseEntity<SupplyDto> getSupply (@PathVariable("id") Long id) {
         if (id == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
@@ -28,31 +32,41 @@ public class SupplyRestController {
         if (supply == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(supply, HttpStatus.OK);
+        return new ResponseEntity<>(supplyMapper.toSupplyDto(supply), HttpStatus.OK);
     }
 
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Supply> saveSupply (@RequestBody @Valid Supply supply) {
+    public ResponseEntity<SupplyDto> saveSupply (@RequestBody @Valid SupplyDto supplyDto) {
         HttpHeaders headers = new HttpHeaders();
-        if (supply == null) {
+        if (supplyDto == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+        Supply supply = supplyMapper.toSupply(supplyDto);
         supplyService.save(supply);
-        return new ResponseEntity<>(supply, headers, HttpStatus.CREATED);
+        return new ResponseEntity<>(supplyMapper.toSupplyDto(supply), headers, HttpStatus.CREATED);
     }
 
-    @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Supply> updateSupply (@RequestBody @Valid Supply supply) {
+    @PutMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SupplyDto> updateSupply (@RequestBody @Valid SupplyDto supplyDetailsDto, @PathVariable("id") Long id) {
         HttpHeaders headers = new HttpHeaders();
-        if (supply == null) {
+        if (supplyDetailsDto == null || id == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+        Supply supply = supplyService.getById(id);
+        Supply supplyDetails = supplyMapper.toSupply(supplyDetailsDto);
+        if (supply == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        supply.setDateOfSupply(supplyDetails.getDateOfSupply());
+        supply.setDishesSupplies(supplyDetails.getDishesSupplies());
+        supply.setSupplier(supplyDetails.getSupplier());
+
         supplyService.save(supply);
-        return new ResponseEntity<>(supply, headers, HttpStatus.OK);
+        return new ResponseEntity<>(supplyMapper.toSupplyDto(supply), headers, HttpStatus.OK);
     }
 
     @DeleteMapping(value = "{id}",  produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Supply> deleteSupply (@PathVariable("id") Long id) {
+    public ResponseEntity<SupplyDto> deleteSupply (@PathVariable("id") Long id) {
         if (id == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
@@ -65,12 +79,12 @@ public class SupplyRestController {
     }
 
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Supply>> getAllSupplies() {
+    public ResponseEntity<List<SupplyDto>> getAllSupplies() {
         List<Supply> supplies = this.supplyService.getAll();
 
         if (supplies.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(supplies, HttpStatus.OK);
+        return new ResponseEntity<>(supplyMapper.toListSupplyDto(supplies), HttpStatus.OK);
     }
 }
