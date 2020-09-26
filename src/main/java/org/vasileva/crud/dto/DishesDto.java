933ce12b6c434a3fecfba92e3a0a0e3981990c02
Elena Vasileva <elenavw@gmail.com>
2020-09-26 15:40:05 +0400

    commit 17 DishesDto add fields

--- src/main/java/org/vasileva/crud/dto/DishesDto.java
+++ src/main/java/org/vasileva/crud/dto/DishesDto.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.vasileva.crud.entity.DishesSupply;
+import org.vasileva.crud.entity.Orders;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,4 +23,8 @@ public class DishesDto {
     private BigDecimal price;
 
     private Integer balance;
+
+    private Set<Orders> ordersOfDishes;
+
+    private Set<DishesSupply> dishesSupplies;
 }
