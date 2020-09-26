f27c2f281b5c98c7eaad4c642da2f4ddc46d819d
Elena Vasileva <elenavw@gmail.com>
2020-09-26 15:22:07 +0400

    commit 14 try to push

--- src/main/java/org/vasileva/crud/mapper/DishesMapper.java
+++ src/main/java/org/vasileva/crud/mapper/DishesMapper.java
@@ -2,7 +2,6 @@ package org.vasileva.crud.mapper;
 
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 import org.vasileva.crud.dto.DishesDto;
 import org.vasileva.crud.entity.Dishes;
 
