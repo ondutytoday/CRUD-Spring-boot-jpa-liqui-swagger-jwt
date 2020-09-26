02efee7c97c5b2711f3e0ee2e74838bf49554ba6
Elena Vasileva <elenavw@gmail.com>
2020-09-26 21:07:19 +0400

    commit 22 finish mappers

--- src/main/java/org/vasileva/crud/mapper/StaffMapper.java
+++ src/main/java/org/vasileva/crud/mapper/StaffMapper.java
@@ -2,8 +2,18 @@ package org.vasileva.crud.mapper;
 
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
+import org.vasileva.crud.dto.StaffDto;
+import org.vasileva.crud.entity.Staff;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 @Component
 public interface StaffMapper {
+    StaffDto toStaffDto(Staff staff);
+
+    Staff toStaff (StaffDto staffDto);
+
+    List<StaffDto> toListStaffDto(List<Staff> staff);
+
 }
--- src/main/java/org/vasileva/crud/mapper/SuppliersMapper.java
+++ src/main/java/org/vasileva/crud/mapper/SuppliersMapper.java
@@ -2,8 +2,18 @@ package org.vasileva.crud.mapper;
 
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
+import org.vasileva.crud.dto.SuppliersDto;
+import org.vasileva.crud.entity.Suppliers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 @Component
 public interface SuppliersMapper {
+
+    SuppliersDto toSuppliersDto(Suppliers supplier);
+
+    Suppliers toSuppliers (SuppliersDto supplierDto);
+
+    List<SuppliersDto> toListSuppliersDto(List<Suppliers> suppliers);
 }
