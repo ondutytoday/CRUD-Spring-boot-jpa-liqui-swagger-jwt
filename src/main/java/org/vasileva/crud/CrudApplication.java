a14e7dcb40d3b6e0166921f5e8f5426d85f307de
Elena Vasileva <elenavw@gmail.com>
2020-09-28 19:51:53 +0400

    initial commit

--- src/main/java/org/vasileva/crud/CrudApplication.java
+++ src/main/java/org/vasileva/crud/CrudApplication.java
@@ -2,14 +2,19 @@ package org.vasileva.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class CrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrudApplication.class, args);
-
-
     }
+
 
 }
