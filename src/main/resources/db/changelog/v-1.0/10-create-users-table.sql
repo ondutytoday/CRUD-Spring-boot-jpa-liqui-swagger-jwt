ed5bf5f8656e47cc2ac9c0fc97bc33e150cbe5d2
Elena Vasileva <elenavw@gmail.com>
2020-10-24 14:53:45 +0400

    Commit 3.1. Liquibase script fix for users-table

--- src/main/resources/db/changelog/v-1.0/10-create-users-table.sql
+++ src/main/resources/db/changelog/v-1.0/10-create-users-table.sql
@@ -1,21 +1,18 @@
-create table table_name
+create table users
 (
 	user_id bigserial not null,
 	username varchar(100) not null,
 	password varchar(255) not null,
 	email varchar(255) not null,
-	role varchar(15) not null
+	role varchar(15) not null,
+	primary key (user_id)
 );
 
 create unique index users_email_uindex
-	on table_name (email);
+	on users (email);
 
 create unique index users_user_id_uindex
-	on table_name (user_id);
+	on users (user_id);
 
 create unique index users_username_uindex
-	on table_name (username);
-
-alter table table_name
-	add constraint users_pk
-		primary key (user_id);
+	on users (username);
