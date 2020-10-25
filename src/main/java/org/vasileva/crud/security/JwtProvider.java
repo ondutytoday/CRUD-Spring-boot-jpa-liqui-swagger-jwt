52f2d6ef518e228c7f6a2e8fc6f6a39b381e349b
Elena Vasileva <elenavw@gmail.com>
2020-10-25 17:08:12 +0400

    Commit 8 JwtProvider

--- src/main/java/org/vasileva/crud/security/JwtProvider.java
+++ src/main/java/org/vasileva/crud/security/JwtProvider.java
@@ -1,17 +1,56 @@
 package org.vasileva.crud.security;
 
+import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Slf4j
 @Component
 public class JwtProvider {
 
-    @Value("$(jwt.secret)")
+    @Value("${jwt.secret}")
     private String jwtSecret;
 
+    public String generateToken (String username) {
+        Date date = Date.from(LocalDate.now()
+                .plusDays(15)
+                .atStartOfDay(ZoneId.systemDefault())
+                .toInstant());
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(date)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
 
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException expEx) {
+            log.error("Token expired");
+        } catch (UnsupportedJwtException unsEx) {
+            log.error("Unsupported jwt");
+        } catch (MalformedJwtException mjEx) {
+            log.error("Malformed jwt");
+        } catch (SignatureException sEx) {
+            log.error("Invalid signature");
+        } catch (Exception e) {
+            log.error("invalid token");
+        }
+        return false;
+    }
+
+    public String getLoginFromToken (String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
 
 }
