0ae1f45b02aeaf84bf934b40fd3f7eb08cc4606e
Elena Vasileva <elenavw@gmail.com>
2020-10-31 19:39:09 +0400

    Initial commit

---
+++ src/test/java/org/vasileva/crud/rest/StaffRestControllerTest.java
@@ -0,0 +1,30 @@
+package org.vasileva.crud.rest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith
+class StaffRestControllerTest {
+
+    @Test
+    void getStaff() {
+    }
+
+    @Test
+    void saveStaff() {
+    }
+
+    @Test
+    void updateStaff() {
+    }
+
+    @Test
+    void deleteStaff() {
+    }
+
+    @Test
+    void getAllStaff() {
+    }
+}
