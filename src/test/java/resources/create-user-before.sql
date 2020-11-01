b39642aeff68c82961eec656e8462f47651d1938
Elena Vasileva <elenavw@gmail.com>
2020-11-01 13:34:45 +0400

    Commit 3

---
+++ src/test/java/resources/create-user-before.sql
@@ -0,0 +1,3 @@
+insert into users (username, password, email, roles_id) values ('admin', '$2y$12$Ye8E6QSYZfRolFkLfrif5eRH93fA4WIpZ5RELYvbULkip8wTifDe2', 'cbooeln0@devhub.com', 1);
+insert into users (username, password, email, roles_id) values ('user1', '$2y$12$Ye8E6QSYZfRolFkLfrif5eRH93fA4WIpZ5RELYvbULkip8wTifDe2', 'pivanchikov1@nbcnews.com', 2);
+insert into users (username, password, email, roles_id) values ('user2', '$2y$12$Ye8E6QSYZfRolFkLfrif5eRH93fA4WIpZ5RELYvbULkip8wTifDe2', 'siwanczyk2@geocities.jp', 2);
